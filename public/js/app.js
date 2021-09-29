4ef6f8dc251e727b0afc40ea9d96dbdec09e1014
Prateek Mishra <justwriteprateek@gmail.com>
2021-09-29 12:25:31 +0530

    displaying username on login

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,60 @@
 
 
-
-const weatherForm = document.querySelector('#form')
-const search = document.querySelector('#input')
-const msg1 = document.querySelector('#msg1')
-const msg2 = document.querySelector('#msg2')
+// import Register from "../components/Register";
+// import Login from "../components/Login";
 
 
-
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-   // console.log('reached here')
-    const location = search.value
-    msg1.textContent = 'Loading.....'
-
-    msg2.textContent = ''
+const form = document.querySelector('#form')
+// // const search = document.querySelector('#input')
+// // const msg1 = document.querySelector('#msg1')
+// // const msg2 = document.querySelector('#msg2')
 
 
-    fetch('/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                msg1.textContent = data.error
-            } else {
-                msg1.textContent = data.location
-                msg2.textContent = data.forecastData
-                // console.log(data.location)
-                // console.log(data.forecastData)
-            }
-        })
-    })
+const temp = document.getElementById("account")
+temp.innerHTML = "prateek"
+console.log("hello")
+
+// if(username){
+//     console.log("prateek")
+// }
+
+
+form.addEventListener('submit', (e) => {
+//    e.preventDefault()
+   console.log('reached here')
 })
+// //     const location = search.value
+// //     msg1.textContent = 'Loading.....'
+
+// //     msg2.textContent = ''
+
+
+// //     fetch('/weather?address=' + location).then((response) => {
+// //         response.json().then((data) => {
+// //             if (data.error) {
+// //                 msg1.textContent = data.error
+// //             } else {
+// //                 msg1.textContent = data.location
+// //                 msg2.textContent = data.forecastData
+// //                 // console.log(data.location)
+// //                 // console.log(data.forecastData)
+// //             }
+// //         })
+// //     })
+// // })
+
+// function App() {
+//     return (
+//         <div className="bg-gray-900 min-h-screen">
+//             <BrowserRouter>
+//                 <Switch>
+//                     <Route component={Register} exact path="/register"/>
+//                     <Route component={Login}  exact path="/login"/>
+//                 </Switch>
+//                 <Footer/>
+//             </BrowserRouter>
+//         </div>
+//     );
+// }
+
+// export default App;
--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const hbs = require('hbs')
 const path = require('path')
 const express = require('express')
+const passport = require('passport');
+const mongoose = require('mongoose');
+const mongo = require('mongodb');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
+//const db = require('./models');
+//const Role = db.role;
+// const geocode = require('./utils/geocode')
+//const mongoClient = require('mongodb').MongoClient;
+// const hbs = require('hbs')
+// const path = require('path')
+// const express = require('express')
 // const geocode = require('./utils/geocode')
 // const forecast = require('./utils/forecast')
+
 
 const app= express();
 const port = process.env.PORT || 5000
@@ -18,26 +34,143 @@ app.set('view engine', 'hbs')
 app.set('views', viewPath)
 hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectory))
+app.use(cors())
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '.../build')))
+}
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.json(), urlencodedParser)
 
+const connection_uri = 'mongodb+srv://kvnp:eB5jtcsFvsRlT6jR@cluster0.pktvg.mongodb.net/healfvr?retryWrites=true&w=majority'
+
+mongoose.connect(connection_uri, {useNewurlParser:true, useUnifiedTopology:true })
+.then((res) => {
+    app.listen(process.env.PORT, () => console.log("dbserver is live"))
+})
+.catch(err => console.log(err))
+
+function verifyJWT(req, res, next) {
+    const token = req. headers["x-access-token"]?.split(' ')[1]
+    if(token) {
+        jwt.verify(token, process.env.PASSPORTSECRET, (err, decoded) => {
+            if(err) return res.json({ isLoggedIn: false, message: "Failed to authenticate"})
+            req.user = {};
+            req.user.id = decoded.id
+            req.user.username = decoded.username
+            next()
+        })
+    }
+    else{
+        res.json({message: "Incorrect Token Given", isLoggedIn: false})
+    }
+}
+
+app.post("/register", async (req,res) => {
+    const user = req.body;
+    const takenUsername = await User.findOne({username: user.username})
+    const takenEmail = await User.findOne({email: user.email})
+    if(takenUsername || takenEmail) {
+        res.json({message: "Username or email has already been taken"})
+    } else {
+        if (user.password === user.confirmPassword) {
+            user.password = await bcrypt.hash(req.body.password, 10)
+
+        const dbUser = new User({username: user.username.toLowerCase(), email: user.email.toLowerCase(), password: user.password})
+        dbUser.save(err => {
+            if (err) {
+                res.status(500).send({ message: err });
+                return;
+            }
+            res.send({ message: "User was registered successfully!" });
+        })
+        res.render('login', {
+            message: 'Registration Complete. Please login to continue.',
+            messageClass: 'alert-success'
+        });
+        } else {
+            res.render('register', {
+                message: 'Password does not match.',
+                messageClass: 'alert-danger'
+            });
+        }
+        
+    }
+})
+
+app.post("/login", (req,res) => {
+    const userLogin = req.body;
+    //const x= (User.findOne({username: userLogin.email}) ||  User.findOne({email: user.email}) )
+    const x= (User.findOne({username: userLogin.username}))
+    x.then(dbUser => {
+        if(!dbUser) {
+            return res.json({message:"Invalid username or password"})
+        }
+        bcrypt.compare(userLogin.password, dbUser.password)
+        .then( isCorrect => {
+            if(isCorrect) {
+                /*
+                const payload = {
+                    id: dbUser._id,
+                    username: dbUser.username,
+                } 
+                jwt.sign(
+                    payload,
+                    process.env.JWT_SECRET,
+                    {expiresIn: 86400},
+                    (err, token) => {
+                        if(err) return res.json({message: err})
+                        return res.json({errmessage: "Sucess", token: "Bearer " + token})
+                    }
+                )*/
+                    
+                res.render('index', {
+                    username: userLogin.username
+                });
+            }
+            else{
+                return res.render('login', {
+                    message: 'Invalid username or password.',
+                    messageClass: 'alert-danger'
+                });
+            }
+        })
+    })
+})
+
+app.get("/getUsername", verifyJWT, (req,res) => {
+    res.json({isLoggedIn: true, username: req.user.username})
+})
 
 app.get('', (req,res)=>{
     res.render('index',{
         title: 'MedicBot',
-        name: 'Prateek'
+        name: 'Prateek',
     })
 })
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+app.get('/login', (req, res) => {
+    res.render('login');
+});
 
 app.get('/history', (req,res)=>{
     res.render('history',{
         title: 'history',
-        name: 'Prateek'
+        name: 'Prateek',
+    })
+})
+app.get('/account', (req,res)=>{
+    res.render('account',{
+        title: 'history',
+        name: 'Prateek',
     })
 })
 
 app.get('/docs', (req,res)=>{
     res.render('docs',{
         title: 'docs',
-        name: 'Prateek'
+        name: 'Prateek',
     })
 })
 
@@ -52,58 +185,11 @@ app.get('/docs', (req,res)=>{
 
 
 
-// app.get('/weather', (req,res)=>{
-//     if(!req.query.address){
-//         return res.send({
-//             error: 'please provide an address'
-//         })
-//     }
-
-//     geocode(req.query.address, (error,{latitude,longitude,location}={})=>{
-//         if(error){
-//             return res.send({
-//                 error
-//             })
-//         }
-//         forecast(latitude, longitude, (error, forecastData)=>{
-//             if(error){
-//                 return res.send({
-//                     error: 'please provide an address'
-//                 })
-//               }
-//             res.send({
-//                 location,
-//                 forecastData
-//             })
-            
-//         })
-//   })
-// })
-
-// app.get('*', (req, res) => {
-//     res.render('404', {
-//         title: '404',
-//         name: 'Prateek Mishra',
-//         errorMessage: 'Error 404, Page not found.'
-//     })
-// })
 
 
 
 
 
-
-
-// app.get('', (req,res)=>{
-//     res.send('<h1>hii this is front page</h1>')
-// })
-
-// app.get('/docs', (req,res)=>{
-//     res.send('hii this is documentation page')
-// })
-// app.get('/contact', (req,res)=>{
-//     res.send('hii this is contact page')
-// })
 
 app.listen(port,()=>{
     console.log('app is running now')
